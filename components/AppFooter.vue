357f94807ab378391e7a8779aba710a213e0d638
ZhangZisu <i@zzs1.cn>
2021-03-14 12:33:39 +0800

    Small fix

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,8 +1,8 @@
 <template>
   <v-footer app>
     <v-row justify="center" no-gutters>
-      <div class="text-overline">
-        &copy; {{ new Date().getFullYear() }} SwarmP Org
+      <div class="text-overline" style="text-transform: none !important">
+        &copy; {{ new Date().getFullYear() }} The SwarmP Team
       </div>
     </v-row>
   </v-footer>
